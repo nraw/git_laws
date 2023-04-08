c9797bdf7691dbda22d804ffbf14aef61792e282
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-04-08 15:49:27 +0200

    cleaner http

--- README.md
+++ README.md
@@ -11,3 +11,48 @@ data
 │       ├── vsebina.bson
 │       └── vsebina.metadata.json
 └── vsebina.tar.gz
+
+
+## Notes:
+
+### mail PISRS
+PISRS ima tudi spletni servis, ki za potrebe ponovne uporabe informacij javnega značaja omogoča prevzem prečiščenih in osnovnih besedil v formatu .pdf, .htm in .doc.
+
+- Za prevzem prečiščenih besedil:
+http://pisrs.si/Pis.web/npbDocPdf?idPredpisa=(ID osnovnga predpisa)&idPredpisaChng=(ID novele)&type=(vrsta zapisa:pdf, doc, html)
+Primer:
+http://pisrs.si/Pis.web/npbDocPdf?idPredpisa=ZAKO8009&idPredpisaChng=ZAKO4697&type=doc
+
+
+- Za prevzem osnovnih besedil:
+http://www.pisrs.si/Pis.web/npbDocPdf?idPredpisa=(ID osnovnega predpisa)&type=(vrsta zapisa:pdf, doc, html)
+Primer:
+http://www.pisrs.si/Pis.web/npbDocPdf?idPredpisa=ZAKO4697&type=doc
+
+Osnovno besedilo Zakona o dohodnini – ZDoh-2 (Uradni list RS, št. 117/06 z dne 16. 11. 2006) je dostopno na:
+http://www.pisrs.si/Pis.web/npbDocPdf?idPredpisa=ZAKO4697&type=doc v .doc formatu
+http://www.pisrs.si/Pis.web/npbDocPdf?idPredpisa=ZAKO4697&type=pdf v .pdf formatu in
+http://www.pisrs.si/Pis.web/npbDocPdf?idPredpisa=ZAKO4697&type=html v .html formatu
+
+### Q: A je slucajno kje mozno dobiti seznam vseh idPredpis ter idPredpisaChng?
+A: na portalu OPSI se nahajajo vsi podatki (tudi NPBji):
+https://podatki.gov.si/publisher/vlada_republike_slovenije_sluzba_za_zakonodajo.
+
+Med osnovnimi podatki je na voljo tudi tabela "vpliva_na", ki prikazuje na kateri predpis posamezen predpisa (ali njegov predlog) vpliva.
+
+Podatki se lahko uparjo z "osnovni" kjer so na ID vezani še podatki o naslovu, vrsti, veljavnosti, objavi itd. predpisa.
+
+### Pravna podlaga za pripravo prečiščenih besedil predpisov je podana:
+
+
+za vse predpise v 1. točki prvega odstavka 10. člena Zakona o dostopu do informacij javnega značaja ( Uradni list RS, št 51/06 – uradno prečiščeno besedilo 2 in 117/06-ZDavP), po kateri je vsak organ dolžan, kot informacijo javnega značaja, posredovati v svetovni splet neuradna prečiščena besedila predpisov, ki se nanašajo na njegovo delovno področje;
+ 
+
+za zakone v 153. členu Poslovnika Državnega zbora, po katerem
+neuradno prečiščeno besedilo, ki se objavi na spletnih straneh Državnega zbora, pripravi Zakonodajno-pravna služba Državnega zbora po vsaki spremembi ali dopolnitvi zakona,
+uradno prečiščeno besedilo, ki se objavi v Uradnem listu Republike Slovenije in na spletnih straneh Državnega zbora, potrdi Državni zbor na podlagi sklepa, ki ga sprejme na predlog matičnega delovnega telesa, Vlade ali poslanske skupine;
+
+
+### Komentar o neuradnih preciscenih besedilih
+neuradna prečiščena besedila, ki so dostopna v pravno-informacijskem sistemu, le informativnega značaja, pravno veljavni pa so le podatki in besedila pravnih aktov, ki so objavljeni v Uradnem listu Republike Slovenije. Neuradna prečiščena besedila predstavljajo le informativni delovni pripomoček, ki sicer pomembno prispeva k preglednosti in uporabnosti predpisov, vendar nima pravne veljavnosti.
+
--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import os
+import re
 from pathlib import Path
 
 import bson
 import pandas as pd
+from bs4 import BeautifulSoup as bs
 from git import Repo
-
-data_location = "/tmp/slovenian_laws/"
-os.mkdir(data_location)
+from loguru import logger
+from markdownify import markdownify
+from tqdm import tqdm
 
 
 def main():
+    # https://podatki.gov.si/dataset/osnovni-podatki-o-predpisih-rs
     vpliva_na = pd.read_csv("data/vplivana.csv")
     osnovni_raw = pd.read_csv("data/osnovni.csv")
     osnovni = osnovni_raw.dropna(subset=["D_SPREJEMA"]).copy()
     osnovni["date_accepted"] = pd.to_datetime(osnovni["D_SPREJEMA"], format="%d.%m.%y")
+    # https://podatki.gov.si/dataset/neuradna-preciscena-besedila-predpisov
     with open("data/vsebina.bson/pisrs/vsebina.bson", "rb") as f:
         laws = bson.decode_all(f.read())
 
     laws_list = [law["idPredpisa"] for law in laws]
     laws_changes = [law["idPredpisaChng"] for law in laws]
-    laws_changes == law_id
 
     law_id = "ZAKO4697"
-    law_code = osnovni[osnovni.ID == law_id].iloc[0].KRATICA
+    law_code = get_law_code(law_id, osnovni)
     affected_ids = vpliva_na[vpliva_na.VPLIVA_NA == law_id]
     affected = osnovni[
         ((osnovni.ID.isin(affected_ids.ID)) & (osnovni.KRATICA.str.contains(law_code)))
@@ -31,8 +34,11 @@ def main():
     affected_laws = affected[affected.ID.str.startswith("ZAKO")].sort_values(
         "date_accepted"
     )
+
+    data_location = "/tmp/slovenian_laws/"
+    os.mkdir(data_location)
     repo = Repo.init(data_location)
-    for _, affected_law_row in affected_laws.iterrows():
+    for _, affected_law_row in tqdm(affected_laws.iterrows()):
         #  affected_law_id = affected_laws["ID"].iloc[0]
         affected_law_id = affected_law_row["ID"]
         affected_law_title = affected_law_row["NASLOV"]
@@ -42,16 +48,47 @@ def main():
         )
         affected_law_date = affected_law_row["date_accepted"]
         affected_law = get_law(affected_law_id, laws)
-        Path(data_location + law_code + ".html").write_text(affected_law["vsebina"])
+        if not affected_law:
+            continue
+        vsebina = affected_law["vsebina"]
+        vsebina_clean = re.sub(r"( |\n|\r)+", " ", vsebina)
+
+        soup = bs(vsebina_clean)  # make BeautifulSoup
+        prettyHTML = soup.prettify()
+        #  vsebina_md = markdownify(vsebina, convert=["style"])
+        #  vsebina_md = re.sub(
+        #      "/(<!--.*?-->)|(<!--[\S\s]+?-->)|(<!--[\S\s]*?$)/g", "", vsebina_md
+        #  )
+        #  Path(data_location + law_code + ".md").write_text(vsebina_md)
+        Path(data_location + law_code + ".html").write_text(prettyHTML)
         repo.git.add(all=True)
         repo.git.commit(date=affected_law_date, m=commit_msg)
 
 
-def get_law(law_id, laws):
+def get_law(affected_law_id, laws):
     laws_list = [law["idPredpisa"] for law in laws]
-    law_number = laws_list.index(law_id)
-    law = laws[law_number]
+    if affected_law_id in laws_list:
+        law_number = laws_list.index(affected_law_id)
+        law = laws[law_number]
+    else:
+        logger.warning(f"No law available for {affected_law_id}")
+        law = ""
     return law
+
+
+def get_law_code(law_id, osnovni):
+    law_code = osnovni[osnovni.ID == law_id].iloc[0].KRATICA
+    return law_code
+
+
+def get_law_id(law_code, osnovni):
+    law_id = osnovni[osnovni.KRATICA == law_code].iloc[0].ID
+    return law_id
+
+
+def get_markdown(vsebina):
+    vsebina_md = markdownify(vsebina)
+    return vsebina_md
 
 
 if __name__ == "__main__":
